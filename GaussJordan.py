b3582276e034a5f69e6d5fc22148a4f178aa8062
Giovanni Guarnieri Soares <giovanniguarnieri@id.uff.br>
2017-08-30 13:40:29 -0300

    Matriz ordenada

--- GaussJordan.py
+++ GaussJordan.py
@@ -10,4 +10,13 @@ x=np.matrix( ((1.,1.,0.,3.,4.), (2.,1.,-1.,1.,1.), (3.,-1.,-1.,2.,-3.), (-1.,2.,
 n=len(x)-1
 print "Matriz antes de tudo:"
 print x
+for i in xrange(n,-1,-1):
+ for ii in xrange(n,-1,-1):
+  if(x.item(i,ii)==0):
+   y=np.copy(x[ii+1])
+   x[ii+1]=np.copy(x[i])
+   x[i]=np.copy(y)
+print "Matriz ordenada: "
+print x
+
 
